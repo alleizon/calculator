6cf7b4d341ed79620faff23f20317e7d51d7ddd1
alleizon <112495304+alleizon@users.noreply.github.com>
2022-12-05 19:01:26 +0200

    added global DOM variables and currentNumber func

---
+++ main.js
@@ -0,0 +1,86 @@
+// DOM variables
+
+const currentText = document.querySelector("#current-text");
+const previousText = document.querySelector("#previous-text");
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const plusMinus = document.querySelector("#plus-minus")
+const clearBtn = document.querySelector("#clear")
+
+let currentNumber = currentText.innerText;
+
+// Event listeners for numbers
+
+const numberBtnsArray = Array.from(numberBtns);
+
+numberBtnsArray.forEach(item => {
+   item.addEventListener('click', updateCurrentField); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateCurrentField(e) {
+    const number = e.target.textContent;
+    let containsDot = currentNumber.includes(".") ? true : false;
+
+    if ((number === '.') && containsDot) return;
+
+    let length;
+    const re = /\s/g;
+    if (containsDot) length = currentNumber.replace(re, '').length-1;
+    else length = currentNumber.replace(re, '').length;
+
+    if (length >= 12) return;
+
+    currentNumber += number;
+    const formattedNumber = formatNumber(currentNumber.replace(re, ''));
+    console.log(currentNumber, currentNumber.replace(re, '').length);
+
+    currentText.innerText = formattedNumber;
+}
+
+function formatNumber(numberString) {
+    let dotIndex;
+
+    if (numberString.includes('.')) dotIndex = numberString.indexOf('.');
+    const afterDot = dotIndex ? numberString.slice(dotIndex) : '';
+    const numberBeforeDot = numberString.slice(0,dotIndex);
+    const numberArray = numberBeforeDot.split('');
+    const length = numberBeforeDot.length;
+    if (length > 3) {
+        const numberOfSpaces = Math.floor(length/3);
+        const firstSpaceIndex = length % 3 ? length % 3 : 3;
+        let newIndex = firstSpaceIndex;
+        let addedSpaces = 0;
+        for (let i = 0; i<numberOfSpaces; i++) {
+            if (i==0) {
+                numberArray.splice(firstSpaceIndex,0,' ');
+                addedSpaces++;
+            }
+            else {
+                numberArray.splice(newIndex+addedSpaces,0,' '); 
+                addedSpaces++
+            }
+
+            newIndex += 3;
+        }
+    }
+    return numberArray.join('')+afterDot;
+}
